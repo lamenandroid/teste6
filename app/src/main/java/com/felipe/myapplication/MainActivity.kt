b4c57ec3e854c32b1a3db53996ab2465684b089a
qualifica-df <mesaone.devmatutino@gmail.com>
2022-06-23 10:54:17 -0300

    Initial commit

---
+++ app/src/main/java/com/felipe/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.felipe.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.felipe.myapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
